069fa3485d3f208a2cf1f0401e10ba6ac39e2dd3
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2025-11-02 03:52:43 -0300

    Botão de retornar

--- app/src/components/header/index.tsx
+++ app/src/components/header/index.tsx
@@ -2,11 +2,12 @@ import Box from "@mui/material/Box"
 import IconButton from "@mui/material/IconButton"
 import Typography from "@mui/material/Typography"
 import Icon from "components/icon"
-import { useMatches } from "react-router"
+import { useMatches, useNavigate } from "react-router"
 import type { RouteLoaderData, RouteMatch } from "typesrc/pages"
 
 export default function Header() {
 
+    const navigate = useNavigate()    
     const matches = useMatches() as RouteMatch[]
 
     const activePage = matches.at(-1)
@@ -23,7 +24,9 @@ export default function Header() {
             className="flex w-full justify-between items-center p-4 gap-4"
         >
             <Box className="w-16">
-                <IconButton>
+                <IconButton
+                    onClick={() => navigate(-1)}
+                >
                     <Icon className="text-white">
                         chevron_left
                     </Icon>
